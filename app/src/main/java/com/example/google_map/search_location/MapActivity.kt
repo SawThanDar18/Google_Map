f56cc146c70939080a2f7753ab5a1087eae4b321
SawThandar18 <sawthandar@ucsy.edu.mm>
2019-06-10 11:04:54 +0630

    show current location on google map

--- app/src/main/java/com/example/google_map/search_location/MapActivity.kt
+++ app/src/main/java/com/example/google_map/search_location/MapActivity.kt
@@ -7,6 +7,9 @@ import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.Manifest
+import android.app.DownloadManager
+import android.content.Context
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.FragmentActivity
 import android.support.v4.content.ContextCompat
 import android.view.View
@@ -33,35 +36,111 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback, LocationListener, Go
  GoogleApiClient.OnConnectionFailedListener{
 
     private var mMap : GoogleMap? = null
-    internal lateinit var mLastLocation : Location
-    internal var mCurrLocationMarker : Marker? = null
     internal var mGoogleApiClient : GoogleApiClient? = null
-    internal lateinit var mLocationRequest: LocationRequest
-
-    private lateinit var search : Button
+    internal var mLocationRequest: LocationRequest? = null
+    internal var mLastLocation : Location? = null
+    internal var mCurrLocationMarker : Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_activity)
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            checkLocationPermission()
+        }
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
-
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        //if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 buildGoogleApiClient()
                 mMap!!.isMyLocationEnabled = true
-            //}
-        }/*else{
-            buildGoogleApiClient()
-            mMap!!.isMyLocationEnabled = true
-        }*/
+        }
+    }
+
+    @Synchronized protected fun buildGoogleApiClient(){
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
+
+        mGoogleApiClient!!.connect()
+    }
+
+    override fun onLocationChanged(location: Location){
+
+        mLastLocation = location
+
+        if(mCurrLocationMarker != null){
+            mCurrLocationMarker!!.remove()
+        }
+
+        //place current location marker
+        val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        markerOptions.title("Myanmar")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        mCurrLocationMarker = mMap!!.addMarker(markerOptions)
+
+        //move map camera
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap!!.animateCamera(CameraUpdateFactory.zoomBy(10f))
+
+        //stop location updates
+        if(mGoogleApiClient != null){
+            LocationServices.getFusedLocationProviderClient(this)
+            //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this)
+        }
+    }
+
+    override fun onConnected(bundle: Bundle?) {
+
+        mLocationRequest = LocationRequest()
+        mLocationRequest!!.interval = 1000
+        mLocationRequest!!.fastestInterval = 1000
+        mLocationRequest!!.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            LocationServices.getFusedLocationProviderClient(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when(requestCode){
+            REQUEST_LOCATION_CODE -> {
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        if(mGoogleApiClient == null){
+                            buildGoogleApiClient()
+                        }
+                        mMap!!.isMyLocationEnabled = true
+                    }
+                }else{
+                    Toast.makeText(this, "Myanmar", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+    }
+
+    fun checkLocationPermission() : Boolean{
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_CODE)
+            }else{
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_CODE)
+            }
+            return  false
+        }else
+            return true
     }
 
     fun searchLocation(view: View) {
@@ -104,60 +183,16 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback, LocationListener, Go
         }
     }
 
-    protected fun buildGoogleApiClient(){
-        mGoogleApiClient = GoogleApiClient.Builder(this)
-            .addConnectionCallbacks(this)
-            .addOnConnectionFailedListener(this)
-            .addApi(LocationServices.API).build()
-
-        mGoogleApiClient!!.connect()
-    }
-
-    override fun onConnected(bundle: Bundle?) {
-
-        mLocationRequest = LocationRequest()
-        mLocationRequest.interval = 1000
-        mLocationRequest.fastestInterval = 1000
-        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            LocationServices.getFusedLocationProviderClient(this)
-        }
-    }
-
     override fun onConnectionSuspended(i: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onLocationChanged(location: Location){
-
-        mLastLocation = location
-
-        if(mCurrLocationMarker != null){
-            mCurrLocationMarker!!.remove()
-        }
-
-        //place current location marker
-        val latLng = LatLng(location.latitude, location.longitude)
-        val markerOptions = MarkerOptions()
-        markerOptions.position(latLng)
-        markerOptions.title("Current Position")
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-        mCurrLocationMarker = mMap!!.addMarker(markerOptions)
-
-        //move map camera
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
-        mMap!!.animateCamera(CameraUpdateFactory.zoomTo(11f))
-
-        //stop location updates
-        if(mGoogleApiClient != null){
-            LocationServices.getFusedLocationProviderClient(this)
-        }
-    }
 
     override fun onConnectionFailed(connectionResult : ConnectionResult) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-
+    companion object{
+        val REQUEST_LOCATION_CODE = 95
+    }
 }
